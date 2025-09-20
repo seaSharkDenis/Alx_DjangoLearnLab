9b115062ec7ff0a23d8e4a8da5e78c4c3a62ed49
Denis Ndiritu <denisndiritu1@gmail.com>
2025-09-20 12:37:54 +0300

    Defined URL patterns in the app's urls.py to the authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth.views import LoginView
-from .views import list_books, LibraryDetailView, LibraryListView, register
+from . import views
+from .views import list_books, LibraryDetailView, LibraryListView
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
@@ -10,7 +11,7 @@ urlpatterns = [
     path('library/<int:pk>', LibraryDetailView.as_view(), name='library-detail'),
 
     # Authentication URLs
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
