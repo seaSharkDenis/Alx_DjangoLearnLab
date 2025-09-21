29fd86216bc75219fcc7cecbd8802ae219cdc3fb
Denis Ndiritu <denisndiritu1@gmail.com>
2025-09-21 23:40:39 +0300

    Implemente user authentication and permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
 from .models import Book
 from .serializers import BookSerializer
 
 
 # Create your views here.
+# Custom permission class
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Custom permission to allow read-only access to all users,
+    but only allow write operations to admin users.
+    """
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to admin users
+        return request.user and request.user.is_staff
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,8 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authentication',
     'api',
 ]
+
+REST_FRAMEWORK = {
+      'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional, for browsable API
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Default permission for all views
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
